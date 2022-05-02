dd44feecd40ca1adfef78b987a211df0644424ed
christian aranda <kristian.aranda@gmail.com>
2022-05-02 15:41:58 +0200

    evolve

--- SpaccioDescans.Web/ViewModels/OrderViewModel.cs
+++ SpaccioDescans.Web/ViewModels/OrderViewModel.cs
@@ -3,4 +3,26 @@
 public class OrderViewModel
 {
     public CustomerInfoViewModel CustomerInfoViewModel { get; set; } = new();
+
+    public ICollection<OrderDetailViewModel> OrderDetailViewModels  { get; set; }  = new List<OrderDetailViewModel>();
+}
+
+public class CustomerInfoViewModel
+{
+    public string Name { get; set; } = string.Empty;
+
+    public string Address { get; set; } = string.Empty;
+
+    public string City { get; set; } = string.Empty;
+
+    public string Phone { get; set; } = string.Empty;
+}
+
+public class OrderDetailViewModel
+{
+    public int ProductCode { get; set; }
+
+    public int Quantity { get; set; }
+
+    public decimal Discount { get; set; }
 }
